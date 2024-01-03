d69decf5f56ebc006d46939197cf522227844098
maxharrisnet <hello@maxharris.codes>
2024-01-03 11:35:40 -0800

    starting WordPress API demo

--- js/cms.js
+++ js/cms.js
@@ -1,73 +1,81 @@
-const cmsURL = 'https://cms.iftheselandscouldtalk.org';
-const apiURL = cmsURL + '/api';
-
 // Fetch page data from Strapi
-function getPageData(page) {
-	fetch(`${apiURL}/${page}?populate=*`, {
-		method: 'GET',
+function getPageData(page, source) {
+	const cms = source;
+	const apiURL = "";
+
+	if (cms === "strapi") {
+		const apiURL = `https://cms.iftheselandscouldtalk.org/api/${page}?populate=*`;
+	} else if (cms === "wordpress") {
+		const apiURL = `https://wp.iftheselandscouldtalk.org/wp-json/wp/v2`;
+	}
+
+	fetch(`${apiURL}`, {
+		method: "GET",
 		headers: {
-			'Content-Type': 'application/json',
+			"Content-Type": "application/json",
 		},
 	})
-		.then((response) => response.json())
-		.then((response) => {
+		.then(response => response.json())
+		.then(response => {
 			const page = response.data;
-			const pageTitle = document.getElementById('page-title');
-			const pageContent = document.getElementById('content-container');
+			const pageTitle = document.getElementById("page-title");
+			const pageContent = document.getElementById("content-container");
 
 			pageTitle.textContent = page.attributes.Title;
 			pageContent.innerHTML = page.attributes.content;
 		})
-		.catch((error) => console.error('Error:', error));
+		.catch(error => console.error("Error:", error));
 }
 
 function getItemData(collection, item) {
 	const urlParams = new URLSearchParams(window.location.search);
-	const slug = urlParams.get('e');
+	const slug = urlParams.get("e");
 	console.log(slug);
 
 	fetch(`${apiURL}/${collection}/${slug}?populate=*`, {
-		method: 'GET',
+		method: "GET",
 		headers: {
-			'Content-Type': 'application/json',
+			"Content-Type": "application/json",
 		},
 	})
-		.then((response) => response.json())
-		.then((response) => {
+		.then(response => response.json())
+		.then(response => {
 			const post = response.data;
-			const postTitle = document.getElementById('post-title');
-			const postContent = document.getElementById('content-container');
+			const postTitle = document.getElementById("post-title");
+			const postContent = document.getElementById("content-container");
 
 			postTitle.textContent = post.attributes.title;
 			postContent.innerHTML = marked.parse(post.attributes.content);
 		})
-		.catch((error) => console.error('Error:', error));
+		.catch(error => console.error("Error:", error));
 }
 
 // Fetch all posts from Strapi
-function getCollectData(collection) {
+function getCollectData(collection, source) {
+	const cms = source;
+
 	fetch(`${apiURL}/${collection}-items?populate=*`, {
-		method: 'GET',
+		method: "GET",
 		headers: {
-			'Content-Type': 'application/json',
+			"Content-Type": "application/json",
 		},
 	})
-		.then((response) => response.json())
-		.then((response) => {
-			response.data.forEach((post) => {
+		.then(response => response.json())
+		.then(response => {
+			response.data.forEach(post => {
 				const container = document.getElementById(`${collection}-container`);
-				const postDiv = document.createElement('div');
-				postDiv.classList.add('post');
-				const postImage = document.createElement('img');
-				const postTitle = document.createElement('h2');
-				const postContent = document.createElement('p');
-				const postLink = document.createElement('a');
+				const postDiv = document.createElement("div");
+				postDiv.classList.add("post");
+				const postImage = document.createElement("img");
+				const postTitle = document.createElement("h2");
+				const postContent = document.createElement("p");
+				const postLink = document.createElement("a");
 
 				// postImage.src = cmsURL + post.attributes.featuredImage.data.attributes.url;
 				postTitle.textContent = post.attributes.title;
 				postContent.innerHTML = marked.parse(post.attributes.content);
-				postLink.innerHTML = 'Read more';
-				postLink.href = '/events/event.html?e=' + post.id;
+				postLink.innerHTML = "Read more";
+				postLink.href = "/events/event.html?e=" + post.id;
 
 				postDiv.appendChild(postTitle);
 				postDiv.appendChild(postImage);
@@ -77,5 +85,5 @@ function getCollectData(collection) {
 				container.appendChild(postDiv);
 			});
 		})
-		.catch((error) => console.error('Error:', error));
+		.catch(error => console.error("Error:", error));
 }
