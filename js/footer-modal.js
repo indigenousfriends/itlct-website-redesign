106a7faf128087d4531b926a47570202481ac9b3
Charli <charlisable@gmail.com>
2024-01-11 16:21:33 -0600

    stopped body scroll for when modal is open, worked on minor spacing fixes

--- js/footer-modal.js
+++ js/footer-modal.js
@@ -3,11 +3,15 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay-la");
 const openModalBtn = document.querySelector(".btn-la");
 const closeModalBtn = document.querySelector(".btn-close");
+const body = document.body;
+
+console.log(body);
 
 // Function for opening the modal
 const openModal = function () {
   modal.classList.remove("hidden");
   overlay.classList.remove("hidden");
+  body.classList.add("fixed");
 };
 // Event listener for opening the modal when the button is clicked
 openModalBtn.addEventListener("click", openModal);
@@ -16,6 +20,7 @@ openModalBtn.addEventListener("click", openModal);
 const closeModal = function () {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
+  body.classList.remove("fixed");
 };
 
 // Event listeners for clicking the close button, clicking the overlay, or pressing Esc key
