6fdf8aa6e63b8bfa9d5f1a1c2d313245d77ce999
maxharrisnet <hello@maxharris.codes>
2024-02-07 18:06:02 -0800

    a bunch of CMS stuff, working on team modals

--- js/our-team-modal.js
+++ js/our-team-modal.js
@@ -10,16 +10,16 @@ console.log(closeModalBtns);
 
 // Function for opening the natasha modal
 const openNatashaModal = function () {
-  natashaModal.classList.remove("hidden");
-  overlayTeam.classList.remove("hidden");
-  bodyTeam.classList.add("fixed");
+	natashaModal.classList.remove("hidden");
+	overlayTeam.classList.remove("hidden");
+	bodyTeam.classList.add("fixed");
 };
 // Function for opening the marsha modal
 const openMarshaModal = function () {
-    marshaModal.classList.remove("hidden");
-    overlayTeam.classList.remove("hidden");
-    bodyTeam.classList.add("fixed");
-  };
+	marshaModal.classList.remove("hidden");
+	overlayTeam.classList.remove("hidden");
+	bodyTeam.classList.add("fixed");
+};
 // Event listener for opening the modal when the natasha button is clicked
 natashaBtn.addEventListener("click", openNatashaModal);
 
@@ -28,22 +28,26 @@ marshaBtn.addEventListener("click", openMarshaModal);
 
 // Function for closing the modal
 const closeModalTeam = function () {
-  natashaModal.classList.add("hidden");
-  marshaModal.classList.add("hidden");
-  overlayTeam.classList.add("hidden");
-  bodyTeam.classList.remove("fixed");
+	natashaModal.classList.add("hidden");
+	marshaModal.classList.add("hidden");
+	overlayTeam.classList.add("hidden");
+	bodyTeam.classList.remove("fixed");
 };
 
 // Event listeners for clicking the close button, clicking the overlay, or pressing Esc key
 // closeModalBtns.addEventListener("click", closeModal);
-closeModalBtns.forEach( (btn) => {
-  btn.addEventListener("click", closeModalTeam);
-})
+closeModalBtns.forEach(btn => {
+	btn.addEventListener("click", closeModalTeam);
+});
 
 overlayTeam.addEventListener("click", closeModalTeam);
 
 document.addEventListener("keydown", function (e) {
-  if (e.key === "Escape" && (!natashaModal.classList.contains("hidden") || !marshaModal.classList.contains("hidden"))) {
-    closeModalTeam();
-  }
+	if (
+		e.key === "Escape" &&
+		(!natashaModal.classList.contains("hidden") ||
+			!marshaModal.classList.contains("hidden"))
+	) {
+		closeModalTeam();
+	}
 });
